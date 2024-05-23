265a1d89984184d2158608c956f507f4fb9c5d5f
Avo Cado <avo.cado@codefirstgirls.com>
2024-05-23 19:26:42 +0100

    Adding a click counter

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ for (let i = 0; i < 10; i ++) {
 }
 
 document.getElementById("message").textContent = result;
+
+let count = 0;
+
+const button = document.getElementById("click_me_button");
+const counterSpan = document.getElementById("counter");
+
+console.log(counterSpan)
+
+button.addEventListener("click", () => {
+    count ++
+    counterSpan.innerText = count
+})
