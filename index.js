b680303e36f434e465b99b302a54beef579d9d07
Avo Cado <avo.cado@codefirstgirls.com>
2024-05-23 19:12:21 +0100

    Inner text and append

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 document.write("Some text added by JavaScript")
+
+const embeddedDiv = document.getElementById("about-me");
+
+console.log(embeddedDiv)
+
+embeddedDiv.innerText = "I am replacing what was there before"
+embeddedDiv.append("Hello World")
+
+console.log(embeddedDiv.textContent)
